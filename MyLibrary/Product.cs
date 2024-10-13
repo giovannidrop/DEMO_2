69e8b9e278bfb2f47cb66785acd0237c52d880f1
Giovanni Saavedra Valdes <giovannisaavedra160@gmail.com>
2024-10-12 23:13:15 -0500

    Commit

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace MyLibrary
+{
+    public class Product(int id,  string name, decimal unitPrice,int unitsInStock)
+    {
+        //propiedades
+        public int Id => Id;
+        public string Name => name;
+        public decimal UnitPrice => unitPrice;
+        public int UnitsInStock => unitsInStock;
+
+        public bool Discontinued { get; private set; }
+
+        public void SetDiscontinued() =>
+            Discontinued = true;
+
+        public bool GetStatus() =>
+            Discontinued;
+    }
+}
